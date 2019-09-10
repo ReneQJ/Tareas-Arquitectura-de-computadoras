17efab9ea468cdf5d930b34f29113ca72d56a3f4
plupyt <55117123+plupyt@users.noreply.github.com>
2019-09-09 22:13:11 -0500

    Update Convertir a decimal.c

--- Convertir a decimal.c
+++ Convertir a decimal.c
@@ -71,13 +71,12 @@ int validar(int k,int baseDeSistema){
 
 //Convertir de decimal a cualquier base
 int decimalToAnyBase(char numeroDecimal[], int baseDeSistema){ //Se ingresa número decimal y la base a la que se desea convertir
-	char residuos[];
 	for (int i=0;numeroDecimal[i]!='\0';i++){
 		//Detecta el largo de la cadena y lo guarda en i
 	}
 	i--; //Se le resta 1 a i para ajustar la cadena
 	for(int j=0; numeroEntrada==0; j++){	//Ciclo que itera hasta que una división dé como resultado 0 (fin de las divisiones) 
-	int residuos[i]; //Cambiar el 5 por el largo de la cadena 
+	char residuos[i]; //se crea el array con el largo de la cadenan (*no sé si es char o int ya que ahí guardará números)
 	residuos[j] = numeroEntrada%baseDeSistema //guarda el residuo de cada division en una cadena
 	numeroEntrada = numeroEntrada/baseDeSistema; //El resultado de la división se vuelve a dividir por la base cada que itera el ciclo
 	}
