2cdac177d6c3f9c78c22ca947dcdb44e62332427
plupyt <55117123+plupyt@users.noreply.github.com>
2019-09-09 22:04:01 -0500

    Update Convertir a decimal.c

--- Convertir a decimal.c
+++ Convertir a decimal.c
@@ -70,12 +70,17 @@ int validar(int k,int baseDeSistema){
 }
 
 //Convertir de decimal a cualquier base
-int decimalToAnyBase(int numeroEntrada, int base){ //Se ingresa número decimal y la base a la que se desea convertir
-	for(int i=0; numeroEntrada==0; i++){	//Ciclo que itera hasta que una división dé como resultado 0 (fin de las divisiones) 
-	int residuos[5]; //Cambiar el 5 por el largo de la cadena 
-	residuos[i] = numeroEntrada%base //guarda el residuo de cada division en una cadena
-	numeroEntrada = numeroEntrada/base; //El resultado de la división se vuelve a dividir por la base cada que itera el ciclo
+int decimalToAnyBase(char numeroDecimal[], int baseDeSistema){ //Se ingresa número decimal y la base a la que se desea convertir
+	char residuos[];
+	for (int i=0;numeroDeOtroSistema[i]!='\0';i++){
+		//Detecta el largo de la cadena y lo guarda en i
 	}
-	for(int j=5; j=0; j--){	//Ciclo que va disminuyendo (el 5 debería ser el largo de la cadena)
-		printf(residuos[j] + " "); //Se imprime cada residuo en orden inverso con un espacio para distinguir cada dígito
+	i--; //Se le resta 1 a i para ajustar la cadena
+	for(int j=0; numeroEntrada==0; j++){	//Ciclo que itera hasta que una división dé como resultado 0 (fin de las divisiones) 
+	int residuos[i]; //Cambiar el 5 por el largo de la cadena 
+	residuos[j] = numeroEntrada%baseDeSistema //guarda el residuo de cada division en una cadena
+	numeroEntrada = numeroEntrada/baseDeSistema; //El resultado de la división se vuelve a dividir por la base cada que itera el ciclo
+	}
+	for(int k=i; k=0; k--){	//Ciclo que va disminuyendo (el 5 debería ser el largo de la cadena)
+		printf(residuos[k] + " "); //Se imprime cada residuo en orden inverso con un espacio para distinguir cada dígito
 }
